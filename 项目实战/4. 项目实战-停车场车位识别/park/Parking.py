fbb095a6fe0f502558def54c84d0f0c85d992988
于海翔 <yoohaythem@gmail.com>
2023-09-13 00:04:51 +0800

    opencv项目实战代码注释 20230913

--- 项目实战/4. 项目实战-停车场车位识别/park/Parking.py
+++ 项目实战/4. 项目实战-停车场车位识别/park/Parking.py
@@ -40,7 +40,7 @@ class Parking:
         self.cv_show('white_mask', white_mask)
 
         # 这行代码使用掩码 white_mask 对输入图像 image 进行按位与（bitwise and）操作，将只保留在指定颜色范围内的部分，其他部分将变为零。
-        # 这样，就可以将白色和黄色区域提取出来，其他区域变为黑色。
+        # 这样，就可以将想要的区域提取出来，其他区域变为黑色。
         masked = cv2.bitwise_and(image, image, mask=white_mask)
         self.cv_show('masked', masked)
         return masked
@@ -184,45 +184,51 @@ class Parking:
             cv2.rectangle(new_image, tup_topLeft, tup_botRight, (0, 255, 0), 3)
         return new_image, rects
 
+    # rects:存放停车位坐标的字典，键为依次递增的数字索引
+    # 在图像中绘制停车位标记线和编号，并返回带有标记的图像和停车位字典。
     def draw_parking(self, image, rects, make_copy=True, color=[255, 0, 0], thickness=2, save=True):
         if make_copy:
             new_image = np.copy(image)
-        gap = 15.5
-        spot_dict = {}  # 字典：一个车位对应一个位置
+        gap = 15.5  # 停车位标记线之间的间隔 gap
+        spot_dict = {}  # 创建一个字典 spot_dict，用于存储每个停车位的坐标范围和编号。
         tot_spots = 0
-        # 微调
+
+        # 微调，这个就是经验值了，可以跳过不影响代码逻辑
         adj_y1 = {0: 20, 1: -10, 2: 0, 3: -11, 4: 28, 5: 5, 6: -15, 7: -15, 8: -10, 9: -30, 10: 9, 11: -32}
         adj_y2 = {0: 30, 1: 50, 2: 15, 3: 10, 4: -15, 5: 15, 6: 15, 7: -20, 8: 15, 9: 15, 10: 0, 11: 30}
-
         adj_x1 = {0: -8, 1: -15, 2: -15, 3: -15, 4: -15, 5: -15, 6: -15, 7: -15, 8: -10, 9: -10, 10: -10, 11: 0}
         adj_x2 = {0: 0, 1: 15, 2: 15, 3: 15, 4: 15, 5: 15, 6: 15, 7: 15, 8: 10, 9: 10, 10: 10, 11: 0}
+
         for key in rects:
             tup = rects[key]
             x1 = int(tup[0] + adj_x1[key])
             x2 = int(tup[2] + adj_x2[key])
             y1 = int(tup[1] + adj_y1[key])
             y2 = int(tup[3] + adj_y2[key])
+            # 绘制矩形框，颜色为绿色 (0, 255, 0)，线宽为 2
             cv2.rectangle(new_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            num_splits = int(abs(y2 - y1) // gap)
+            num_splits = int(abs(y2 - y1) // gap)  # 计算停车位中标记线的数量 num_splits。
             for i in range(0, num_splits + 1):
                 y = int(y1 + i * gap)
-                cv2.line(new_image, (x1, y), (x2, y), color, thickness)
-            if key > 0 and key < len(rects) - 1:
-                # 竖直线
+                cv2.line(new_image, (x1, y), (x2, y), color, thickness)  # 绘制水平的停车位标记线
+            if 0 < key < len(rects) - 1:
+                # 如果停车位不是第一个或最后一个，还绘制垂直的停车位标记线。
                 x = int((x1 + x2) / 2)
                 cv2.line(new_image, (x, y1), (x, y2), color, thickness)
-            # 计算数量
+            # 根据停车位类型（第一个、中间、最后），计算停车位总数 tot_spots。
             if key == 0 or key == (len(rects) - 1):
                 tot_spots += num_splits + 1
             else:
                 tot_spots += 2 * (num_splits + 1)
 
-            # 字典对应好
+            # 将每个停车位的坐标范围和编号存储在 spot_dict 字典中。
+            # 如果停车位是第一个或最后一个，只存储水平标记线的信息。
             if key == 0 or key == (len(rects) - 1):
                 for i in range(0, num_splits + 1):
                     cur_len = len(spot_dict)
                     y = int(y1 + i * gap)
                     spot_dict[(x1, y, x2, y + gap)] = cur_len + 1
+            # 如果是中间的停车位，同时存储水平和垂直标记线的信息。
             else:
                 for i in range(0, num_splits + 1):
                     cur_len = len(spot_dict)
@@ -230,48 +236,60 @@ class Parking:
                     x = int((x1 + x2) / 2)
                     spot_dict[(x1, y, x, y + gap)] = cur_len + 1
                     spot_dict[(x, y, x2, y + gap)] = cur_len + 2
-
+        # 打印总停车位数。
         print("total parking spaces: ", tot_spots, cur_len)
+        # 如果设置了 save 为 True，将带有停车位标记的图像保存为文件 with_parking.jpg。
+        # 返回带有标记的新图像 new_image 和停车位字典 spot_dict。
         if save:
             filename = 'with_parking.jpg'
             cv2.imwrite(filename, new_image)
         return new_image, spot_dict
 
+    # 根据停车位字典中的坐标范围，在图像上绘制停车位的矩形标记，并返回带有标记的图像。
     def assign_spots_map(self, image, spot_dict, make_copy=True, color=[255, 0, 0], thickness=2):
         if make_copy:
             new_image = np.copy(image)
+        # 对于停车位字典中的每个停车位（表示为 (x1, y1, x2, y2)），执行以下操作：
+        # 使用 cv2.rectangle 绘制矩形框，颜色为红色 (255, 0, 0)，线宽为 thickness。
         for spot in spot_dict.keys():
             (x1, y1, x2, y2) = spot
             cv2.rectangle(new_image, (int(x1), int(y1)), (int(x2), int(y2)), color, thickness)
         return new_image
 
+    # 将图像中的每个停车位按照其坐标从原图像中裁剪出来，然后将裁剪后的停车位图像保存到指定文件夹中，以便用于卷积神经网络 (CNN) 的训练数据。
     def save_images_for_cnn(self, image, spot_dict, folder_name='cnn_data'):
         for spot in spot_dict.keys():
             (x1, y1, x2, y2) = spot
             (x1, y1, x2, y2) = (int(x1), int(y1), int(x2), int(y2))
             # 裁剪
             spot_img = image[y1:y2, x1:x2]
+            # 将停车位图像放大两倍，以便适应训练数据的需求。
             spot_img = cv2.resize(spot_img, (0, 0), fx=2.0, fy=2.0)
             spot_id = spot_dict[spot]
-
+            # 为每个停车位生成一个文件名，命名规则为 'spot' + 停车位编号 + '.jpg'。
             filename = 'spot' + str(spot_id) + '.jpg'
             print(spot_img.shape, filename, (x1, x2, y1, y2))
-
+            # 使用 cv2.imwrite 函数将裁剪后的停车位图像保存到指定的文件夹 folder_name 中。
             cv2.imwrite(os.path.join(folder_name, filename), spot_img)
 
+    # 使用训练好的深度学习模型对输入的图像进行分类预测。
     def make_prediction(self, image, model, class_dictionary):
-        # 预处理
+        # 将输入的图像 image 像素值缩放到 [0, 1] 范围内，以便与模型的训练数据相匹配。这是通过将图像的每个像素值除以 255 来实现的。
         img = image / 255.
 
-        # 转换成4D tensor
+        # 将处理后的图像转换成一个4D张量。这是因为深度学习模型通常接受批量图像作为输入，即使我们只有一张图像也需要将其包装成批量。
         image = np.expand_dims(img, axis=0)
 
         # 用训练好的模型进行训练
+        # 调用模型的 predict 方法，传入处理后的图像作为输入，获取模型的分类预测结果。
         class_predicted = model.predict(image)
+        # 从分类预测结果中找到具有最高概率的类别索引 inID。
         inID = np.argmax(class_predicted[0])
+        # 然后根据类别字典 class_dictionary 找到对应的类别标签 label。
         label = class_dictionary[inID]
         return label
 
+    # 使用深度学习模型在输入图像上进行目标检测和分类，并在图像上标记出识别出的停车位，同时计算并显示可用停车位和总停车位的数量。
     def predict_on_image(self, image, spot_dict, model, class_dictionary, make_copy=True, color=[0, 255, 0], alpha=0.5):
         if make_copy:
             new_image = np.copy(image)
@@ -279,27 +297,29 @@ class Parking:
         self.cv_show('new_image', new_image)
         cnt_empty = 0
         all_spots = 0
-        for spot in spot_dict.keys():
+        for spot in spot_dict.keys():  # 对于每个停车位，执行以下操作：
             all_spots += 1
+            # 下面三行提取图像区域：根据停车位的坐标从输入图像中提取出停车位的图像区域。
             (x1, y1, x2, y2) = spot
             (x1, y1, x2, y2) = (int(x1), int(y1), int(x2), int(y2))
             spot_img = image[y1:y2, x1:x2]
+            # 调整图像大小：将停车位的图像区域调整为固定大小（48x48像素），以便输入到深度学习模型进行分类。
             spot_img = cv2.resize(spot_img, (48, 48))
-
+            # 使用模型进行分类：调用 make_prediction 函数，使用深度学习模型对停车位的图像区域进行分类，获取分类标签。
             label = self.make_prediction(spot_img, model, class_dictionary)
             if label == 'empty':
+                # 如果分类标签为 'empty'，表示停车位为空，那么在 overlay 图像上以绿色（BGR）标记该停车位。
                 cv2.rectangle(overlay, (int(x1), int(y1)), (int(x2), int(y2)), color, -1)
                 cnt_empty += 1
 
+        # 将 overlay 图像叠加到 new_image 上，以显示标记的停车位。
         cv2.addWeighted(overlay, alpha, new_image, 1 - alpha, 0, new_image)
 
+        # 计算可用停车位和总停车位数量：统计分类为 'empty' 的停车位数量和所有停车位的数量，并在图像上显示这两个统计信息。
         cv2.putText(new_image, "Available: %d spots" % cnt_empty, (30, 95),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (255, 255, 255), 2)
-
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
         cv2.putText(new_image, "Total: %d spots" % all_spots, (30, 125),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (255, 255, 255), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
         save = False
 
         if save:
@@ -309,42 +329,50 @@ class Parking:
 
         return new_image
 
+    # 使用深度学习模型在输入视频上进行目标检测和分类，标记出识别出的停车位，同时计算并显示可用停车位和总停车位的数量。
     def predict_on_video(self, video_name, final_spot_dict, model, class_dictionary, ret=True):
+        # 使用OpenCV的VideoCapture函数打开指定的视频文件。
         cap = cv2.VideoCapture(video_name)
         count = 0
         while ret:
-            ret, image = cap.read()
+            ret, image = cap.read()  # 通过cap.read()函数读取下一帧的图像。
             count += 1
-            if count == 5:
+            if count == 5:  # 使用计数器（count）来控制处理的频率。在这里，每5帧处理一次。也可以写作：if not count%5:
                 count = 0
 
+                # 复制图像：创建输入图像的副本 new_image 和一个用于叠加标记的图像副本 overlay。
                 new_image = np.copy(image)
                 overlay = np.copy(image)
+                # 初始化变量：初始化用于统计可用停车位和总停车位数量的变量。
                 cnt_empty = 0
                 all_spots = 0
                 color = [0, 255, 0]
                 alpha = 0.5
+                # 遍历停车位字典：对于每个停车位，执行以下操作：
                 for spot in final_spot_dict.keys():
                     all_spots += 1
+                    # 下面三行：提取停车位的图像区域。
                     (x1, y1, x2, y2) = spot
                     (x1, y1, x2, y2) = (int(x1), int(y1), int(x2), int(y2))
                     spot_img = image[y1:y2, x1:x2]
+                    # 调整图像大小，以便输入到深度学习模型进行分类。
                     spot_img = cv2.resize(spot_img, (48, 48))
-
+                    # 使用模型进行分类：调用 make_prediction 函数，使用深度学习模型对停车位的图像区域进行分类，获取分类标签。
                     label = self.make_prediction(spot_img, model, class_dictionary)
                     if label == 'empty':
+                        # 如果分类标签为 'empty'，表示停车位为空，那么在 overlay 图像上以绿色（BGR）标记该停车位。
                         cv2.rectangle(overlay, (int(x1), int(y1)), (int(x2), int(y2)), color, -1)
                         cnt_empty += 1
 
+                # 使用 cv2.addWeighted 函数将 overlay 图像叠加到 new_image 上，以显示标记的停车位。这里是0.5:0.5
                 cv2.addWeighted(overlay, alpha, new_image, 1 - alpha, 0, new_image)
-
+                # 在图像上（视频的帧）显示可用停车位和总停车位数量。
                 cv2.putText(new_image, "Available: %d spots" % cnt_empty, (30, 95),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.7, (255, 255, 255), 2)
-
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
                 cv2.putText(new_image, "Total: %d spots" % all_spots, (30, 125),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.7, (255, 255, 255), 2)
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+
+                # 使用 cv2.imshow 函数显示处理后的图像，并等待按键事件。
                 cv2.imshow('frame', new_image)
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
--- 项目实战/4. 项目实战-停车场车位识别/park/park_test.py
+++ 项目实战/4. 项目实战-停车场车位识别/park/park_test.py
@@ -10,41 +10,60 @@ from keras.preprocessing import image
 from Parking import Parking
 import pickle
 
+# 这行代码用于获取当前工作目录（Current Working Directory，缩写为 cwd），它会将当前 Python 脚本所在的文件夹的路径存储在 cwd 变量中。
 cwd = os.getcwd()
 
 
 def img_process(test_images, park):
+    # ---Python教学---：map 函数接受两个参数：一个函数（park.select_rgb_white_yellow）和一个可迭代对象（test_images），
+    #                 然后将函数应用于可迭代对象中的每个元素，并返回结果的迭代器。
     white_yellow_images = list(map(park.select_rgb_white_yellow, test_images))
     park.show_images(white_yellow_images)
 
+    # 将彩色图像转换为灰度图像。
     gray_images = list(map(park.convert_gray_scale, white_yellow_images))
     park.show_images(gray_images)
 
+    # 用于在灰度图像上检测边缘。默认低阈值50，高阈值200。
+    # ---Python教学---：使用了一个匿名函数（lambda 函数），该函数代表着将 park.detect_edges 函数应用于一个可迭代对象中的每个元素，并返回这些作用完的元素。
+    #              也就相当于 def xxx(image):
+    #                           park.detect_edges(image)
+    #                           return image
+    #                       map(lambda image: park.detect_edges(image), gray_images) ==> map(xxx(image), gray_images)
+    # 这里由于detect_edges函数返回cv2.Canny，只是在图像上做了修改，并没有单独的返回值，所以需要用匿名函数将返回值包装为图像
     edge_images = list(map(lambda image: park.detect_edges(image), gray_images))
     park.show_images(edge_images)
 
+    # 将图像中想要的区域保留，其余部分置为黑色。
     roi_images = list(map(park.select_region, edge_images))
     park.show_images(roi_images)
 
+    # 检测图像中的直线线段的列表。
     list_of_lines = list(map(park.hough_lines, roi_images))
 
-    line_images = []
+    line_images = []  # 存放图片
+    # ---Python教学---：同时遍历两个列表 zip([a1,a2,a3],[b1,b2,b3]) ==>  ([a1,b1],[a2,b2],[a3,b3])
+    # test_images-->white_yellow_images-->gray_images-->gray_images-->edge_images-->roi_images-->list_of_lines，
+    # 所以是test_images和list_of_lines里的元素是一一对应的，可以直接zip
     for image, lines in zip(test_images, list_of_lines):
-        line_images.append(park.draw_lines(image, lines))
-    park.show_images(line_images)
+        line_images.append(park.draw_lines(image, lines))  # 在图上画出筛选后的线段
+    park.show_images(line_images)  # 画多张子图，整合成一张整图
 
     rect_images = []
     rect_coords = []
     for image, lines in zip(test_images, list_of_lines):
         new_image, rects = park.identify_blocks(image, lines)
-        rect_images.append(new_image)
-        rect_coords.append(rects)
+        rect_images.append(new_image)  # 在图像上表示矩形车位
+        rect_coords.append(rects)  # 一个rects存放了一张图上的所有矩形车位的坐标
 
     park.show_images(rect_images)
 
     delineated = []
     spot_pos = []
+    # 依旧是一一对应，rect_coords里的每个元素存放了一张图上的所有矩形车位的坐标
     for image, rects in zip(test_images, rect_coords):
+        # 在图像中绘制停车位标记线和编号，并返回带有标记的图像和停车位字典。
+        # 将每个停车位的坐标范围和编号存储在 spot_dict 字典中。
         new_image, spot_dict = park.draw_parking(image, rects)
         delineated.append(new_image)
         spot_pos.append(spot_dict)
@@ -54,39 +73,47 @@ def img_process(test_images, park):
     print(len(final_spot_dict))
 
     with open('spot_dict.pickle', 'wb') as handle:
+        # 保存车位坐标字典到名为 "spot_dict.pickle" 的文件中。
         pickle.dump(final_spot_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+    # # 将图像中的每个停车位按照其坐标从原图像中裁剪出来，然后将裁剪后的停车位图像保存到指定文件夹中，以便用于卷积神经网络 (CNN) 的训练数据。
     park.save_images_for_cnn(test_images[0], final_spot_dict)
 
     return final_spot_dict
 
 
+# 该函数用于加载预训练的Keras模型。它接受一个参数 weights_path，该参数是指预训练模型的权重文件的路径。
 def keras_model(weights_path):
+    # 这是Keras中的一个函数，用于从指定路径加载一个已经保存的模型，包括模型的架构和权重。
+    # 传递给函数的 weights_path 参数是模型权重文件的路径，该文件包含了模型的学习参数。
     model = load_model(weights_path)
+    # 函数返回加载的模型
     return model
 
 
 def img_test(test_images, final_spot_dict, model, class_dictionary):
     for i in range(len(test_images)):
+        # 使用深度学习模型在输入图像上进行目标检测和分类，并在图像上标记出识别出的停车位，同时计算并显示可用停车位和总停车位的数量。
         predicted_images = park.predict_on_image(test_images[i], final_spot_dict, model, class_dictionary)
 
 
 def video_test(video_name, final_spot_dict, model, class_dictionary):
     name = video_name
-    cap = cv2.VideoCapture(name)
+    cap = cv2.VideoCapture(name)  # 这一行意义不明
+    # 使用深度学习模型在输入视频上进行目标检测和分类，标记出识别出的停车位，同时计算并显示可用停车位和总停车位的数量。
     park.predict_on_video(name, final_spot_dict, model, class_dictionary, ret=True)
 
 
 if __name__ == '__main__':
     # 使用glob模块的glob函数，它接受一个文件路径模式（通配符）作为参数，返回匹配模式的文件列表。
     # 使用Matplotlib库的imread函数，它用于读取图像文件并将其加载为NumPy数组。path参数是文件的路径。
-    test_images = [plt.imread(path) for path in glob.glob('test_images/*.jpg')]   # 列表推导式
+    test_images = [plt.imread(path) for path in glob.glob('test_images/*.jpg')]  # 列表推导式
     weights_path = 'car1.h5'  # 训练好的模型
     video_name = 'parking_video.mp4'
     class_dictionary = {}
     class_dictionary[0] = 'empty'
     class_dictionary[1] = 'occupied'
-    park = Parking()   # 封装了所有需要用到的函数的一个类
+    park = Parking()  # 封装了所有需要用到的函数的一个类
     park.show_images(test_images)
     final_spot_dict = img_process(test_images, park)
     model = keras_model(weights_path)
